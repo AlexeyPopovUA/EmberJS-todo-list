6666717848a59220b4b8c47c0f68de21fd04c78c
AlexeyPopovUA <alexejpopovua@gmail.com>
2015-06-20 19:25:00 +0300

    Mocked task list

--- app/controllers/tasks.js
+++ app/controllers/tasks.js
@@ -1,5 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  taskList: "tasks list"
+  taskList: [
+    {
+      name: "name1",
+      description: "Description1"
+    },
+    {
+      name: "name2",
+      description: "Description2"
+    }
+  ]
 });
